3267fd94e1c5bb77a26fa6eec3dc2a2e17b49d3f
Felix Mattick <felix.mattick@gmail.com>
2019-01-21 18:19:05 -0500

    Add upload whole project

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 const ora = require('ora')
 const request = require('request-promise-native')
 const chalk = require('chalk')
+const fs = require('promise-fs')
+const pathLib = require('path')
 
 let spinner = ora('Getting token').start()
 const jar = request.jar()
@@ -14,8 +16,15 @@ const transform = (body, response) => {
   }
 }
 const tokenRegex = /(?<="id":")([a-f0-9-]{30,})(?=")/g
+const ignored = [
+  'node_modules',
+  'yarn.lock',
+  '.gitignore',
+  '.git'
+]
 
 let createdREPL = null
+let writeURI = null
 
 request({
   uri: 'https://repl.it/languages/nodejs',
@@ -27,26 +36,53 @@ request({
   transform
 }).then(({ body, uri }) => {
   spinner.succeed()
-  spinner = ora('Getting write handler').start()
+  spinner = ora('Uploading').start()
   createdREPL = uri
+  const indexExists = fs.existsSync('index.js')
+  if (!indexExists) {
+    spinner.clear()
+    console.log(chalk.yellow('Your project doesn\'t have an index.js file, please rename your main file'))
+  }
   const id = tokenRegex.exec(body)[0]
-  return request({
-    uri: `https://repl.it/data/repls/signed_urls/${id}/index.js`,
-    jar
-  })
-}).then((body) => {
-  spinner.succeed()
-  spinner = ora('Uploading code...').start()
-  const actions = JSON.parse(body)['urls_by_action']
-  const writeURI = actions['write']
-  return request.put({
-    uri: writeURI,
-    body: `console.log('Hello, world!')`,
-    jar
-  }, 'print("Hello!")')
+  return upload('.', id)
 }).then(() => {
   spinner.succeed()
   console.log(chalk.green(`Created REPL: ${createdREPL.href}`))
-}).catch(() => {
+}).catch((error) => {
   spinner.fail()
+  console.log(chalk.red(error.message))
 })
+
+const upload = (rootPath, id) => {
+  return fs.readdir(rootPath).then((paths) => {
+    const promises = []
+    for (let path of paths) {
+      if (!fs.existsSync(path) || ignored.includes(pathLib.basename(path))) {
+        continue
+      }
+      promises.push(fs.lstat(path).then((stats) => {
+        if (stats.isDirectory()) {
+          return upload(pathLib.join(rootPath, path), id)
+        } else {
+          spinner.text = 'Getting write handler'
+          return request({
+            uri: `https://repl.it/data/repls/signed_urls/${id}/` + path,
+            jar
+          }).then((body) => {
+            spinner.text = 'Uploading'
+            const actions = JSON.parse(body)['urls_by_action']
+            writeURI = actions['write']
+            return fs.readFile(path)
+          }).then((code) => {
+            return request.put({
+              uri: writeURI,
+              body: code.toString(),
+              jar
+            })
+          })
+        }
+      }))
+    }
+    return Promise.all(promises)
+  })
+}
