9c00be7527374acf06f178f241cfae9b14b564c4
Felix Mattick <felix.mattick@gmail.com>
2019-01-22 23:34:36 -0500

    Fix project running

--- index.js
+++ index.js
@@ -33,6 +33,8 @@ const referrer = 'https://repl.it/@FelixMattick'
 
 let createdREPL = null
 let id = null
+let indexExists = false
+let webREPL = false
 
 request({
   uri: 'https://repl.it/languages/nodejs',
@@ -46,23 +48,39 @@ request({
   spinner.succeed()
   spinner = ora('Uploading').start()
   createdREPL = uri
-  const indexExists = fs.existsSync('index.js')
+  indexExists = fs.existsSync('index.js')
   if (!indexExists) {
     spinner.clear()
-    console.log(chalk.yellow('Your project doesn\'t have an index.js file, please rename your main file'))
+    console.log(chalk.yellow('Your project doesn\'t have an index.js file, please rename your main file to enable running'))
   }
   id = tokenRegex.exec(body)[0]
+  spinner.clear()
+  console.log(chalk.blue(id, createdREPL.href))
   return upload('.',)
 }).then(() => {
   spinner.succeed()
-  return run()
+  if (indexExists) {
+    return run()
+  } else {
+    return Promise.resolve()
+  }
 }).then(() => {
   spinner.succeed()
-  console.log(chalk.green('Done!', createdREPL.href))
+  console.log(chalk.green('Project URL:', createdREPL.href))
+  if (webREPL) {
+    console.log(chalk.green('Web URL:', toWebREPL(createdREPL.href)))
+  }
 }).catch((error) => {
   spinner.fail()
   console.log(chalk.red(error.message))
 })
+
+const toWebREPL = (url) => {
+  let id = url.replace(/((^https?:\/\/)?repl.it\/repls\/)|(\/$)/g, '')
+  id = id.toLowerCase()
+  id = 'https://' + id + '--five-nine.repl.co'
+  return id
+}
 
 const upload = (rootPath,) => {
   return fs.readdir(rootPath).then((paths) => {
@@ -103,17 +121,32 @@ const run = () => {
     uri: `https://repl.it/data/repls/${id}/gen_repl_token`,
     headers: {
       'Referrer': referrer
-    }
+    },
+    jar
   }).then((body) => {
     return JSON.parse(body)
   }).then((token) => {
-    let output = ''
+    let stage = 0
+    let finished = false
+    let installingPackages = false
     return new Promise((resolve, reject) => {
+      let setMainTimeout
       spinner.succeed()
       spinner = ora('Connecting to websocket').start()
       const ws = new Sockette('wss://eval.repl.it/ws', {
         timeout: 5e3,
         onopen: () => {
+          setMainTimeout = () => {
+            setTimeout(() => {
+              if (!finished) {
+                spinner.clear()
+                console.log(chalk.blue('Reached 8 second timeout, so ending output'))
+                finished = true
+                ws.close()
+                resolve()
+              }
+            }, 8000)
+          }
           ws.json({
             command: 'auth',
             data: token
@@ -123,29 +156,56 @@ const run = () => {
           if (event.type === 'message') {
             const { command, data, error } = JSON.parse(event.data)
             if (command === 'ready') {
-              spinner.succeed()
-              spinner = ora('Running').start()
-              ws.json({
-                command: 'interpRun',
-                data: ''
-              })
+              if (stage === 0) {
+                spinner.succeed()
+                spinner = ora('Running').start()
+                stage++
+                ws.json({
+                  command: 'stop',
+                  data: ''
+                })
+              } else if (stage === 1) {
+                stage++
+                ws.json({
+                  command: 'reset',
+                  data: ''
+                })
+              } else if (stage === 2) {
+                ws.json({
+                  command: 'eval',
+                  data: fs.readFileSync('index.js').toString()
+                })
+                setTimeout(() => {
+                  if (!installingPackages) {
+                    setMainTimeout()
+                  }
+                }, 1000)
+              }
             } else if (command === 'event:packageInstallStart') {
               spinner.text = 'Installing packages'
             } else if (command === 'event:packageInstallOutput') {
-              spinner.clear()
-              console.log(chalk.dim(format(data)))
+              if (data.trim()) {
+                spinner.clear()
+                console.log(chalk.dim(format(data.trim())))
+              }
             } else if (command === 'event:packageInstallEnd') {
               spinner.succeed()
               spinner = ora('Running').start()
-            } else if (command === 'event:interpOutput') {
+              setMainTimeout()
+            } else if (command === 'output') {
               if (data.trim() && data.charCodeAt(0) !== 27) {
                 spinner.clear()
                 console.log(chalk.dim(format(stripAnsi(data.trim()))))
               }
-            } else if (command === 'event:interpPrompt') {
+            } else if (command === 'event:portOpen') {
+              spinner.clear()
+              console.log(chalk.blue('A port opened, so ending output'))
+              finished = true
+              webREPL = true
               ws.close()
               resolve()
             } else if (error) {
+              finished = true
               ws.close()
               reject({
                 message: format(error)
